0c08877cb87b3198719d375647126ef0f9d7a350
Kimmo Lepola <kimmo.lepola@gmail.com>
2019-09-24 13:25:08 +0300

    blog list 00

---
+++ blog_list/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const middleware = require('./utils/middleware');
+const blogsRouter = require('./controllers/blogs');
+const config = require('./utils/config');
+
+const app = express();
+
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
+
+app.use(cors());
+app.use(express.static('build'));
+app.use(bodyParser.json());
+app.use(middleware.requestLogger);
+
+app.use('/api/blogs', blogsRouter);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
